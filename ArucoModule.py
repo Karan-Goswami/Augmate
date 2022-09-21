95b0034bae00a0c0a3b82e8ba8def06d96324ff3
ayushgupta-repo <ayushgupta.8848@gmail.com>
2022-09-21 20:10:40 +0530

    Creating aruco markers detection function not completed

--- ArucoModule.py
+++ ArucoModule.py
@@ -3,6 +3,26 @@ import cv2.aruco as aruco
 import numpy as np
 import os
 
+from symbol import parameters
+
+# creating function to find arucomarkers from the camera
+
+# markersSize = 6 means 6*6
+def findArucoMarkers(img, markersSize=6, totalMarkers=250, draw=True):
+
+    # converting image from BGR to GRAY
+    imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # getting size based on variables
+    key = getattr(aruco, f'DICT_{markersSize}X{markersSize}_{totalMarkers}')
+
+    arucoDict = aruco.Dictionary_get(key)
+
+    arucoParam = aruco.DetectorParameters_create()
+
+    # getting boundingboxes, ids, rejected_markers
+    bboxs, ids, rejected = aruco.detectMarkers(imgGray, arucoDict, parameters=arucoParam)
+
 # main function
 def main():
     # enabling video capture device pass 0 for in-built laptops webcam
